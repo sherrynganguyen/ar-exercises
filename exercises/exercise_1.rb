f4f878294f663adf812f1f6c2731c08ad1299524
Nga Nguyen <ngasdragon@gmail.com>
2019-12-09 17:51:42 -0800

    exercise 1 & 2

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,23 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+Store.create do |store|
+  store.name = "Burnaby"
+  store.annual_revenue = 300000
+  store.mens_apparel = true
+  store.womens_apparel = true
+end
+
+Store.create do |store|
+  store.name = "Richmond"
+  store.annual_revenue = 1260000
+  store.mens_apparel = false
+  store.womens_apparel = true
+end
+
+Store.create do |store|
+  store.name = "Gastown"
+  store.annual_revenue = 190000
+  store.mens_apparel = true
+  store.womens_apparel = false
+end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,2 @@
 class Store < ActiveRecord::Base
-
 end
