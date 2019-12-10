7bf78fd75461cc4ce462ad4c1c3aca6475c63949
Nga Nguyen <ngasdragon@gmail.com>
2019-12-10 10:22:17 -0800

    remove comments

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -11,12 +11,7 @@ puts "----------"
 
 # Your code goes here ...
 
-# Ask the user for a store name (store it in a variable)
 puts "Enter store name"
 user_input = gets.chomp
 @new_store = Store.create(name: user_input)
 @new_store.errors.messages.each { |key,message| puts " #{key}: #{message[0]}" }
-
-# Attempt to create a store with the inputted name but leave out the other fields (annual_revenue, mens_apparel, and womens_apparel)
-
-# Display the error messages provided back from ActiveRecord to the user (one on each line) after you attempt to save/create the record
