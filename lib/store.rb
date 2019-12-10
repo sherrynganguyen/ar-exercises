28543fab09f2c2f94fedff77acbbd84f73200deb
Nga Nguyen <ngasdragon@gmail.com>
2019-12-10 11:39:53 -0800

    finish ex7

--- lib/store.rb
+++ lib/store.rb
@@ -2,5 +2,10 @@ class Store < ActiveRecord::Base
   has_many :employees
   validates :name, length: { minimum: 3 }
   validates :annual_revenue, numericality: {greater_than_or_equal_to: 0 }
-  # validates :
+  validate :product_line, on: :create
+  def product_line
+    if mens_apparel == false && womens_apparel == false
+      errors.add(:product_line, "need to carry at least one product line")
+    end
+  end
 end
